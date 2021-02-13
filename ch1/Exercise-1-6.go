b5f108f7198ce90ed2a4f97038d0979c5d08f2e8
geek-LHW <1141921007@qq.com>
2021-02-14 04:00:24 +0800

    Exercise 1.6

---
+++ ch1/Exercise-1-6.go
@@ -0,0 +1,53 @@
+// 练习 1.6： 修改Lissajous程序，修改其调色板来生成更丰富的颜色，
+// 然后修改SetColorIndex的第三个参数，看看显示结果吧。
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+	"time"
+)
+
+//定义一个slice切片变量,复合声明
+var palette = []color.Color{color.White, color.RGBA{0, 255, 0, 255}, color.RGBA{255, 0, 255, 255}}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	lissajous(os.Stdout)
+}
+
+//定义函数
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			index := rand.Intn(2) + 1
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				uint8(index))
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+
+}
