7d83996a923055d502861f0164988794a6e046b0
geek-LHW <1141921007@qq.com>
2021-02-14 04:02:44 +0800

    Exercise 1.10

---
+++ ch1/Exercise-1-10.go
@@ -0,0 +1,53 @@
+// 练习 1.10： 找一个数据量比较大的网站，用本小节中的程序调研网站的缓存策略，
+// 对每个URL执行两遍请求，查看两次时间是否有较大的差别，并且每次获取到的响应内容是否一致，
+// 修改本节中的程序，将响应结果输出，以便于进行对比。
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		fmt.Printf("start fetch %v\n", url)
+		go fetch(url, ch) // start a goroutine
+		go fetch(url, ch)
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // receive from channel ch
+		fmt.Println(<-ch) // receive from channel ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close() // don't leak resources
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
+}
+
+// ./Exercise-1-10.exe http://www.baidu.com  http://taobao.com
+//output
+// 0.24s   297681  http://www.baidu.com
+// 0.29s   297647  http://www.baidu.com
+// 0.73s   120997  http://taobao.com
+// 0.80s   120997  http://taobao.com
+// 0.81s elapsed
