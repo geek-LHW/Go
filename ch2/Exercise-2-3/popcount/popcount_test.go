4a24bdeb2a14d9cbb60f7c0495361d412cafe346
geek-LHW <1141921007@qq.com>
2021-02-16 16:57:11 +0800

    done exercise-2-3

---
+++ ch2/Exercise-2-3/popcount/popcount_test.go
@@ -0,0 +1,39 @@
+package popcount
+
+import (
+	"testing"
+)
+
+var x uint64 = 0x1234567890ABCDEF
+
+// BenchmarkPopCount-8 2000000000 0.65 ns/op
+func BenchmarkPopCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCount(x)
+	}
+}
+
+// BenchmarkPopCountLoop-8 100000000 17.6 ns/op
+func BenchmarkPopCountLoop(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountLoop(x)
+	}
+}
+
+// output
+// go test -bench=BenchmarkPopCount
+// goos: windows
+// goarch: amd64
+// pkg: github.com/geek-LHW/go/ch2/Exercise-2-3/popcount
+// BenchmarkPopCount-8             1000000000               0.265 ns/op
+// BenchmarkPopCountLoop-8         70633522                15.1 ns/op
+// PASS
+// ok      github.com/geek-LHW/go/ch2/Exercise-2-3/popcount        1.572s
+
+// go test -bench=BenchmarkPopCountLoop
+// goos: windows
+// goarch: amd64
+// pkg: github.com/geek-LHW/go/ch2/Exercise-2-3/popcount
+// BenchmarkPopCountLoop-8         74868979                15.1 ns/op
+// PASS
+// ok      github.com/geek-LHW/go/ch2/Exercise-2-3/popcount        1.316s
