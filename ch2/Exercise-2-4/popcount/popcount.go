85ebbb7b18515f9f847220bf63bfaf16c082e258
geek-LHW <1141921007@qq.com>
2021-02-16 17:25:56 +0800

    add comments

--- ch2/Exercise-2-4/popcount/popcount.go
+++ ch2/Exercise-2-4/popcount/popcount.go
@@ -1,3 +1,6 @@
+// Exercise 2.4: Write a version of PopCount that counts bits by shifting its argument through 64 bit positions, testing the rightmost bit each time.
+// Compare its performance to the table-  lookup version.
+
 package popcount
 
 // pc[i] is the population count of i.
