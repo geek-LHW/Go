d15d1f944a7cc7f616d2ecda50a814ad24ad66c0
geek-LHW <1141921007@qq.com>
2021-02-16 17:32:20 +0800

    Modify comments

--- ch2/Exercise-2-4/popcount/popcount_test.go
+++ ch2/Exercise-2-4/popcount/popcount_test.go
@@ -6,14 +6,12 @@ import (
 
 var x uint64 = 0x1234567890ABCDEF
 
-// BenchmarkPopCount-8 2000000000 0.65 ns/op
 func BenchmarkPopCount(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		PopCount(x)
 	}
 }
 
-// BenchmarkPopCountLoop-8 100000000 17.6 ns/op
 func BenchmarkPopCountShift(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Shift(x)
